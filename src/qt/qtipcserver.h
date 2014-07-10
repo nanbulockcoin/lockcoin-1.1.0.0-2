4092cfb0dd6b65bb719d1420570a578496e1f489
root <root@ubuntu.(none)>
2014-07-10 21:56:46 +0200

    initial version

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define LockCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "LockCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
